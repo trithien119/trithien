702020422615fc0c91918c117561707cf7bed168
trithien <huynhtrithien119@gmail.com>
2026-04-22 10:13:39 +0700

    game doan so

---
+++ HocScanner.java
@@ -0,0 +1,14 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class HocScanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap So Nguyen :");
+		int a = sc.nextInt();
+	}
+
+}
